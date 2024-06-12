ce41645ee91bff0caa4c744ec859e28f22480edd
VasileS <VasileS@gilat.com>
2024-06-12 10:23:04 +0300

    Server logic progress

--- ahooo-yet-hub/src/main/java/student/examples/ServerApp.java
+++ ahooo-yet-hub/src/main/java/student/examples/ServerApp.java
@@ -10,36 +10,62 @@ import student.examples.config.Configuration;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ServerApp {
     private static final Logger logger = LoggerFactory.getLogger(ServerApp.class);
+    private Set<Map<String, Object>> connections;
+    private ServerSocket serverSocket;
 
-    public static void main(String[] args) {
-        logger.info("Started");
+    public ServerApp(int port) throws IOException {
+        create(port);
+    }
 
-        try (ServerSocket serverSocket = new ServerSocket(Configuration.PORT)) {
-                Socket clientSocket = serverSocket.accept();
-                ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
-                ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+    public void create(int port) throws IOException {
+        connections = new HashSet<>();
+        serverSocket = new ServerSocket(port,0, InetAddress.getLocalHost());
+    }
 
-                    ServerCommand turnOnCommand = new ServerCommand(CommandType.TURN_ON);
-                    oos.writeObject(turnOnCommand);
-                    oos.flush();
+    private void listen() throws IOException {
+        while (true){
+            Socket clientSocket = serverSocket.accept();
+            logger.info(String.format("SERVER: Client connected from ip: %s",clientSocket.getInetAddress()));
+            Map<String,Object> client = new HashMap<>();
 
-                    ClientCommand clientTurnOnCommand = (ClientCommand) ois.readObject();
-
-                    if (clientTurnOnCommand.getType().equals(CommandType.ACKNOWLEDGE)) {
-                        logger.info("Operation on client turn ON done!");
-                    } else {
-                        logger.warn("Error while trying to turn ON client!");
-                    }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            client.put("socket",client);
+            connections.add(client);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        logger.info("SERVER: Initializing");
+        ServerApp app = new ServerApp(10000);
+
+        logger.info("SERVER: Starting");
+        app.listen();
+        logger.info("SERVER: Shutting down");
+
+
+//        ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+//        ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+//
+//        ServerCommand turnOnCommand = new ServerCommand(CommandType.TURN_ON);
+//        oos.writeObject(turnOnCommand);
+//        oos.flush();
+//
+//        ClientCommand clientTurnOnCommand = (ClientCommand) ois.readObject();
+
+//        if (clientTurnOnCommand.getType().equals(CommandType.ACKNOWLEDGE)) {
+//            logger.info("Operation on client turn ON done!");
+//        } else {
+//            logger.warn("Error while trying to turn ON client!");
+//        }
     }
 }
